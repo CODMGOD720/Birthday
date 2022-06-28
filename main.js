8fcb8636a1042be94bbdc42e3795cd3710e8e886
CODMGOD720 <101869442+CODMGOD720@users.noreply.github.com>
2022-06-28 19:52:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var x= document.getElementById("myAudio");
 
 function new_image()
 {
-fabric.Image.fromURL('BirthdayImage' , function(Img)) {
+fabric.Image.fromURL('BirthdayImage.jpg' , function(Img) {
 block_image_object=Img
 block_image_object.scaleToWidth(700);
 block_image_object.scaleToHeight(510);
@@ -12,8 +12,8 @@ top:0,
 left:0
 });
 canvas.add(block_image_object);
-};
-
+});
+}
 function playSound(){
 x.play();
 }
