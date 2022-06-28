65b2a137afa4d877406dc669c0b9ba0bfa4997b9
CODMGOD720 <101869442+CODMGOD720@users.noreply.github.com>
2022-06-28 19:06:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ fabric.Image.fromURL('BirthdayImage' , function(Img)) {
 block_image_object=Img
 block_image_object.scaleToWidth(700);
 block_image_object.scaleToHeight(510);
-block_image_object.set
+block_image_object.set({
 top:0,
 left:0
-};
+});
 canvas.add(block_image_object);
 };
 
 function playSound(){
-x.play('bdayaudio.mp3');
+x.play();
 }
